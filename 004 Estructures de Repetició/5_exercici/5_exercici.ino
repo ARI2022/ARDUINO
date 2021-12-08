9ee874888eeb08fede89c5810e57bf3590ed68cd
gabrieliwan2021 <gabriel.jan.50@lacetania.cat>
2021-12-08 14:53:12 +0100

    for i while

---
+++ 004 Estructures de Repetició/5_exercici/5_exercici.ino
@@ -0,0 +1,237 @@
+/*""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""                                                                                    ""
+""                                                                                    "" 
+""                                 TÍTOL: HELLO WORLD                                 ""
+""                                 GABRIEL JAN IWAN                                   ""
+""                                                                                    ""
+""                                                                                    ""
+""                                                                                    ""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""*/
+
+//"""""""""""""""""""""""""""""""""""" INCLUDE """""""""""""""""""""""""""""""""""""""""
+
+
+
+
+//"""""""""""""""""""""""""""""""""""" VARIABLES """""""""""""""""""""""""""""""""""""""
+
+int taula2 = 2;
+int taula3 = 3;
+int taula4 = 4;
+int taula5 = 5;
+int taula6 = 6;
+int taula7 = 7;
+int taula8 = 8;
+int taula9 = 9;
+int taula10 = 10;
+
+
+
+//""""""""""""""""""""""""""""""""""""" SETUP """"""""""""""""""""""""""""""""""""""""""
+
+void setup()                                 // CONFIGURA EL FINAL DE SORTIDA 
+{
+  //taula del 2 original anterior
+Serial.begin(9600); // set up Serial library at 9600 bps
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula2);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula2);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula2*i);
+}
+
+//taula del 3 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula3);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula3);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula3*i);
+}
+
+//taula del 4 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula4);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula3);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula4*i);
+}
+
+//taula del 5 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula5);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula5);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula5*i);
+}
+
+//taula del 6 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula6);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula6);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula6*i);
+}
+
+//taula del 7 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula7);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula7);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula7*i);
+}
+
+//taula del 8
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula8);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula8);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula8*i);
+}
+
+//taula del 9 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula9);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula9);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula9*i);
+}
+
+//taula del 10 
+Serial.println("  "); //espai per a q sigui mes net
+Serial.print("Taula de multiplicar del ");
+Serial.println(taula10);
+for (int i=0; i <= 10; i++)
+{
+delay(100);
+Serial.print(taula10);
+Serial.print(" x ");
+Serial.print(i);
+Serial.print( " = ");                  
+Serial.println(taula10*i);
+}
+
+//anem a provar una cosa
+Serial.println("  ");
+Serial.print("t");
+delay(500);
+Serial.print("o");
+delay(500);
+Serial.print("t");
+delay(500);
+Serial.print("e");
+delay(500);
+Serial.print("s");
+delay(500);
+Serial.print(" l");
+delay(500);
+Serial.print("e");
+delay(500);
+Serial.print("s");
+delay(500);
+Serial.print(" t");
+delay(500);
+Serial.print("a");
+delay(500);
+Serial.print("u");
+delay(500);
+Serial.print("l");
+delay(500);
+Serial.print("e");
+delay(500);
+Serial.println("s");
+delay(500);
+
+Serial.print("g");
+delay(500);
+Serial.print("u");
+delay(500);
+Serial.print("a");
+delay(500);
+Serial.print("p");
+delay(500);
+Serial.print("o");
+delay(500);
+Serial.print(" e");
+delay(500);
+Serial.print("l");
+delay(500);
+Serial.print(" q");
+delay(500);
+Serial.print("u");
+delay(500);
+Serial.print("e");
+delay(500);
+Serial.print(" l");
+delay(500);
+Serial.print("o");
+delay(500);
+Serial.print(" l");
+delay(500);
+Serial.print("e");
+delay(500);
+Serial.print("a");
+delay(500);
+Serial.print("   :");
+delay(500);
+Serial.print(")");
+delay(500);
+                                       
+}
+
+
+//""""""""""""""""""""""""""""""""""""" LOOP """""""""""""""""""""""""""""""""""""""""""
+ 
+void loop() {                                          //INICIA EL PROGRAMA EN BUCLE
+
+                                       
+}
+
+
+//""""""""""""""""""""""""""""""""""""" FUNCIONS """"""""""""""""""""""""""""""""""""""""
