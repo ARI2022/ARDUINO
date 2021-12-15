b028a41a653a75315d09bdc80a5b12f5e8822fbc
gabrieliwan2021 <gabriel.jan.50@lacetania.cat>
2021-12-15 19:59:46 +0100

    ok

--- 002 array 8 leds/exercici_8/exercici_8.ino
+++ 002 array 8 leds/exercici_8/exercici_8.ino
@@ -15,39 +15,37 @@
 
 //"""""""""""""""""""""""""""""""""""" VARIABLES """""""""""""""""""""""""""""""""""""""
 
-const byte led0 = 5;          // donar nom al pin 5 de l’Arduino
-const byte buttonPin = 2;     // donar nom al pin 2 de l’Arduino
-byte buttonState = 0;         // per guardar l’estat en que és troba el button
-
-
-
+const byte ledPin[] = {5, 6, 7, 8};  // donar nom en un array als pins 5, 6, 7 i 8
+byte ledNum = 4;  // definir variable de número de leds
+unsigned long pausa = 1000;  // definir variable de temps en ms
 
 
 //""""""""""""""""""""""""""""""""""""" SETUP """"""""""""""""""""""""""""""""""""""""""
 
 void setup()                                 // CONFIGURA EL FINAL DE SORTIDA 
 {
-                  
- pinMode(led0, OUTPUT);     // definir el pin 5 com una sortida
-  pinMode(buttonPin, INPUT); // definir el pin 2 com una entrada 
+  for(byte i = 0; i < ledNum; i++) // definir els pins 5, 6, 7 i 8 com sortides
+   {
+    pinMode(ledPin[i], OUTPUT);
+   }                
+
                                        
 }
 
 
 //""""""""""""""""""""""""""""""""""""" LOOP """""""""""""""""""""""""""""""""""""""""""
  
-void loop() {                                          //INICIA EL PROGRAMA EN BUCLE
-  buttonState = digitalRead(buttonPin); //llegir l’estat del button i gardar-lo
-  if (buttonState == 0) //polsador premut, muntat amb Pull-up
-{
-  digitalWrite(led0, HIGH); //codi
-}
-else //polsador no premut, muntat amb Pull-up
-{
-  digitalWrite(led0, LOW);  //codi
-}
-delay(200); //per no estar llegint button molt ràpid
-
+void loop() 
+{                                          //INICIA EL PROGRAMA EN BUCLE
+  for(int i = 0; i < 16; i++) // mostrar del 0 al 15
+   {
+  for(int j = 0; j < ledNum ; j++) // actualitzar estat leds per mostrar número
+    {
+       digitalWrite(ledPin[j], bitRead(i, j));
+    }
+         delay(pausa); // es queden leds pausa ms en aquest estat
+    }
+                                       
 }
 
 
